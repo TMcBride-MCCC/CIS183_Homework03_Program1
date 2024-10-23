b454a4d7a94c4247b767d16f461d21dd62c422fb
TMcBride-MCCC <tmcbride2@my.monroeccc.edu>
2024-10-23 09:52:56 -0400

    Bottom Nav
    Bottom nav is responsive.
    
    Next: Have it hop intents.

--- app/src/main/java/com/example/cis183_homework03_program1/MainActivity.java
+++ app/src/main/java/com/example/cis183_homework03_program1/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.cis183_homework03_program1;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity
     ListView lv_j_main_listOfStudents;
     static ArrayList<Student> listOfStudents = new ArrayList<>();
     HomeListAdapter adapter_home;
+    BottomNavigationView bnv_j_main_bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity
 
         //Connect JAVA to GUI
         lv_j_main_listOfStudents = findViewById(R.id.lv_v_main_students);
+        bnv_j_main_bottomNav = findViewById(R.id.bottomNavigationView);
 
         //Make a new instance of the database
         dbHelper = new DatabaseHelper(this);
@@ -40,6 +46,8 @@ public class MainActivity extends AppCompatActivity
         listOfStudents = dbHelper.fillArrayList();
         //Fill the lv
         fillListView();
+        //Call function for navigation
+        bottomNavListener();
     }
 
     private void checkTableCounts()
@@ -53,5 +61,36 @@ public class MainActivity extends AppCompatActivity
     {
         adapter_home = new HomeListAdapter(this, listOfStudents);
         lv_j_main_listOfStudents.setAdapter(adapter_home);
+    }
+
+    private void bottomNavListener()
+    {
+        bnv_j_main_bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+                int navItem = item.getItemId();
+
+                if (navItem == R.id.nav_home)
+                {
+                    Log.d("NAV ", "Home button clicked");
+                }
+                else if (navItem == R.id.nav_addStudent)
+                {
+                    Log.d("NAV ", "Add Student button clicked");
+                }
+                else if (navItem == R.id.nav_addMajor)
+                {
+                    Log.d("NAV ", "Add Major button clicked");
+                }
+                else if (navItem == R.id.nav_searchStudents)
+                {
+                    Log.d("NAV ", "Search button clicked");
+                }
+
+                return false;
+            }
+        });
     }
 }
